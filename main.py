abaeb2ea7b690c374b8ea7e26aeb605d80dddb58
Heitor Modro <101368252+HeitorModro@users.noreply.github.com>
2022-05-27 00:50:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, flash, render_template 
+
+app = Flask('app')
+app.config['SECRET_KEY'] = 'secret'
+
+@app.route('/')
+def index():
+  return ('/flashing')
+
+
+@app.route('/flashing')
+def flasing():
+  flash('Sucesso', 'success')
+  return render_template('index.html')
+  
+
+if __name__ == '__main__':
+  app.run(host= '0.0.0.0', port=8080)
